4141bc120d65ac0ce884c3b245a38a961524b918
tanAutomationControl <tanb1912976@student.ctu.edu.vn>
2023-03-02 18:17:09 +0700

    add

---
+++ car_dirve_web/js/script.js
@@ -0,0 +1,19 @@
+// var firebaseConfig = {
+//     apiKey: "AIzaSyBCmk3r6cdX7yZACmcGQyNR7vp9P2Wu0qM",
+//     authDomain: "testputgethtml.firebaseapp.com",
+//     databaseURL: "https://testputgethtml-default-rtdb.asia-southeast1.firebasedatabase.app",
+//     projectId: "testputgethtml",
+//     storageBucket: "testputgethtml.appspot.com",
+//     messagingSenderId: "1027337300110",
+//     appId: "1:1027337300110:web:91743d365245cc062541df",
+//     measurementId: "G-M6KM6V14CD"
+// };
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+$("#slider1").roundSlider({
+  radius:100,
+  with:5,
+  handleSize:20,
+  sliderType :"min-range",
+  value:83,
+});
