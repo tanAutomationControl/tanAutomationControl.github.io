ef10a55a6595636031076cd2c7fc272f40553561
tanAutomationControl <tanb1912976@student.ctu.edu.vn>
2023-03-03 15:11:38 +0700

    add

--- car_dirve_web/js/script.js
+++ car_dirve_web/js/script.js
@@ -10,24 +10,47 @@ var firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
-
+var PWM = 0;
 function rangeSlider (value){
-  document.getElementById('rangeValue').innerHTML
+  PWM = document.getElementById('rangeValue').innerHTML
   =value;
-  console.log(value);
-  firebase.database().ref().child('PWM').set(Number(value));
 }
 function rangeSlider2 (value){
   document.getElementById('rangeValue2').innerHTML
   =value;
-  console.log(value);
-    firebase.database().ref().child('PWM2').set(65+Number(value));
+  firebase.database().ref().child('PWM2').set(65-Number(value));
 }
+    
 function btn (){
-    firebase.database().ref().child('PWM').set(0);
-    console.log("btn");
+    firebase.database().ref().child('PWM').set(-Number(PWM));
+    setTimeout(firebase.database().ref().child('PWM').set(0),2000);
+    console.log("btn1");
 }
+
 function btn2 (){
     firebase.database().ref().child('PWM2').set(72);
     console.log("btn2");
   }
+function btn5 (){
+    firebase.database().ref().child('PWM').set(Number(PWM));
+    console.log("btn3");
+  }
+  var btn3 = document.getElementById('btn3');
+  var btn4 = document.getElementById('btn4');
+  btn3.addEventListener('touchstart', function(e){
+    console.log("start1");
+    firebase.database().ref().child('PWM').set(Number(PWM));
+
+  });
+  btn3.addEventListener('touchend', function(e){
+    console.log("end1");
+    firebase.database().ref().child('PWM').set(0);
+  });
+  btn4.addEventListener('touchstart', function(e){
+    console.log("start2");
+    firebase.database().ref().child('PWM').set(Number(PWM));
+  });
+  btn4.addEventListener('touchend', function(e){
+    console.log("end2");
+    firebase.database().ref().child('PWM').set(0);
+  });
